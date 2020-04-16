4711285ba97a946d44f965036a1ae892856cbc72
Bohdan Borodulin <bohdan.borodulin@outlook.com>
2020-04-16 14:22:27 +0300

    Add 3 labs

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        public static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            const int N = 4;
+            int k = 0;
+            int[,] matrixA = new int[N, N];
+
+            int[,] matrixB = new int[N,N];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    k++;
+                    if (i == j)
+                        matrixA[i, j] = N - i;
+                    else
+                        matrixA[i, j] = 0;
+                }
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (i <= j)
+                        matrixB[i,j] = rnd.Next(1, 10);
+                    else
+                        matrixB[i, j]= 0;
+                }
+            }
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(matrixA[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(matrixB[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(k);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ThreadLab1/ThreadLab1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadLab1
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Thread t=new Thread(WriteY);
+        //    t.Start();
+
+        //    for (int i = 0; i < 50; i++)
+        //    {
+        //        Console.WriteLine("x");
+        //    }
+        //}
+
+        //static void WriteY()
+        //{
+        //    for (int i = 0; i < 50; i++)
+        //    {
+        //        Console.WriteLine("y");
+        //    }
+        //}
+
+        //task2
+        //static void Main()
+        //{
+        //    Console.WriteLine("Main thread has id:" + Thread.CurrentThread.ManagedThreadId);
+
+        //    new Thread(Go).Start();
+        //    Go();
+        //}
+
+
+        //static void Go()
+        //{
+        //    for (int cycles = 0; cycles < 5; cycles++)
+        //        Console.WriteLine($"P {Thread.CurrentThread.ManagedThreadId}");
+        //}
+
+        //task3
+
+        //class ThreadTest
+        //{
+        //    bool done = false;
+        //    static void Main()
+        //    {
+        //        ThreadTest tt = new ThreadTest();
+        //        new Thread(tt.Go).Start();
+        //        tt.Go();
+        //    }
+        //    void Go()
+        //    {
+        //        Console.WriteLine($"Thread witch call the function {Thread.CurrentThread.ManagedThreadId}");
+        //        if (!done)
+        //        {
+        //            done = true; Console.WriteLine($"Done with thread {Thread.CurrentThread.ManagedThreadId}");
+        //        }
+        //    }
+        //}
+
+        class ThreadTest
+        {
+            static bool done; // Статическое поле, разделяемое потоками
+
+            static void Main()
+            {
+                new Thread(Go).Start();
+                Go();
+            }
+
+            //    static void Go()
+            //    {
+            //        Console.WriteLine($"Thread witch call the function {Thread.CurrentThread.ManagedThreadId}");
+            //        if (!done) { done = true; Console.WriteLine("Done"); }
+            //    }
+            //} 
+            static void Go()
+            {
+                if (!done)
+                {
+                    Console.WriteLine($"Thread witch call the function {Thread.CurrentThread.ManagedThreadId}");
+                    Console.WriteLine("Done");
+                    done = true;
+                }
+            }
+        }
+    }
+}
